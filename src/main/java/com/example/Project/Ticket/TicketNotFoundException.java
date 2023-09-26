1176d5831557d79e0c82195242791e599bc9ce64
facerman01 <jinhengtay.2022@scis.smu.edu.sg>
2023-09-26 20:38:41 +0800

    added ticket

---
+++ src/main/java/com/example/Project/Ticket/TicketNotFoundException.java
@@ -0,0 +1,18 @@
+package com.example.Project.Ticket;
+
+import org.bson.types.ObjectId;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND) // 404 Error
+
+public class TicketNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public TicketNotFoundException(String id) {
+        super("Could not find ticket " + id);
+    }
+    // public TicketNotFoundException(String eventName) {
+    //     super("Could not find ticket " + eventName);
+    // }
+}
---
+++ src/main/java/com/example/Project/Ticket/TicketRepository.java
@@ -0,0 +1,23 @@
+package com.example.Project.Ticket;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+// import com.example.Ticketing.Ticket.Ticket;
+import org.yaml.snakeyaml.events.Event.ID;
+
+@Repository
+public interface TicketRepository extends MongoRepository<Ticket, String> {
+	List<Ticket> findByEventId(ObjectId eventId) ;
+    Optional<Ticket> findByIdAndEventId(String ticketId, ObjectId eventId);
+
+    //find ticket by category
+    Optional<List<Ticket>> findByEventIdAndCategory(ObjectId objectId, int category);
+    Optional<Ticket> findByEventIdAndSeatNum(ObjectId eventId, int seat_num);
+    Ticket findByEventIdAndSeatNumAndCategory(ObjectId eventId, int seatNum, int category);
+    Optional<List<Ticket>> findByEventIdAndCategoryAndSold(ObjectId objectId, int category, boolean sold);
+
+}
