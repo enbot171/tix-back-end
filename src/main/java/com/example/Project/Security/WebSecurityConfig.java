91538bd97819fe77d8e817ce7847c9396589e8e2
Gerald Ang <Gerald.ang.2022@scis.smu.edu.sg>
2023-11-08 01:52:47 +0800

    Revert "Revert "Revert "updates to src"""
    This reverts commit b4cfc95a3fb0196871a16c9b32b4bd4ac502cf44.

--- src/main/java/com/example/Project/Security/WebSecurityConfig.java
+++ src/main/java/com/example/Project/Security/WebSecurityConfig.java
@@ -78,7 +78,6 @@ public class WebSecurityConfig{
                                 // .requestMatchers("/api/v1/events/getEventByNameDate/{eventName}/{eventDate}/ticketByCategory/{category}/allSeats/{seatNum}/purchase/{userId}").permitAll()
                                 // .requestMatchers("/api/v1/purchases/**").permitAll()
                                 .anyRequest().authenticated()
-                                // .anyRequest().permitAll()
                 );
 
         http.authenticationProvider(authenticationProvider());
